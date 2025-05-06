bd2073dd787a767e802719356420abb3eefd9cb3
Jack Lindamood <jlindamood@anthropic.com>
2025-05-06 15:11:56 -0700

    fix: fix lints and update

--- fallbackmetrics.go
+++ fallbackmetrics.go
@@ -12,15 +12,15 @@ type fallbackMetrics struct {
 }
 
 func (f *fallbackMetrics) Success(ctx context.Context, _ time.Time, duration time.Duration) {
-	f.wrappedSingletons.histogramRecord(ctx, "fallback.success", f.m.fallbackSuccess, duration)
+	f.histogramRecord(ctx, "fallback.success", f.m.fallbackSuccess, duration)
 }
 
 func (f *fallbackMetrics) ErrFailure(ctx context.Context, _ time.Time, duration time.Duration) {
-	f.wrappedSingletons.histogramRecord(ctx, "fallback.error", f.m.fallbackErrFail, duration)
+	f.histogramRecord(ctx, "fallback.error", f.m.fallbackErrFail, duration)
 }
 
 func (f *fallbackMetrics) ErrConcurrencyLimitReject(ctx context.Context, _ time.Time) {
-	f.wrappedSingletons.counterIncr(ctx, "fallback.concurrency_limit", f.m.fallbackErrConcurrencyLimitReject)
+	f.counterIncr(ctx, "fallback.concurrency_limit", f.m.fallbackErrConcurrencyLimitReject)
 }
 
 var _ circuit.FallbackMetrics = &fallbackMetrics{}
